1a5814113ff5a072a9fa6929401fc59b2d5e1916
TextBox75 <ogonzalezp256@gmail.com>
2026-09-26 12:44:33 +0100

    Cleared develop to create next kata repetition

--- src/main/java/software/ulpgc/katas/Main.java
+++ src/main/java/software/ulpgc/katas/Main.java
@@ -2,7 +2,6 @@ package software.ulpgc.katas;
 
 public class Main {
     static void main() {
-        Student student = new Student("Pepito", "Grillo");
-        System.out.println(student.gradePercentage(20, 500));
+
     }
 }
--- src/main/java/software/ulpgc/katas/Student.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.katas;
-
-public record Student(String firstName, String lastName) {
-    public int gradePercentage(float grade, float maxGrade) {
-        return Math.round(grade / maxGrade * 100);
-    }
-}
